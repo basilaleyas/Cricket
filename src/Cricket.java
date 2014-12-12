6ed2843e6b034672c029ff28ad2ffdd8500698fd
basilaleyas <baleyas@gmail.com>
2014-12-11 20:48:54 -0500

    Cricket Files

---
+++ src/Cricket.java
@@ -0,0 +1,11 @@
+import javax.swing.JFrame;
+
+public class Cricket {
+
+	public static void main(String[] args) {
+		JFrame frame = new JFrame("Cricket");
+		frame.setSize(450,450);
+		frame.setVisible(true);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+}
