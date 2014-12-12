857f09b1aec941d6925f7243d4361f55d6f56a86
basilaleyas <baleyas@gmail.com>
2014-12-11 21:32:27 -0500

    Update Cricket.java

--- src/Cricket.java
+++ src/Cricket.java
@@ -1,11 +1,26 @@
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.RenderingHints;
+import java.awt.geom.Ellipse2D;
 import javax.swing.JFrame;
+import javax.swing.JPanel;
 
-public class Cricket {
-
+public class Cricket extends JPanel {
+	
+	public void paint(Graphics g) {
+		Graphics2D g2d = (Graphics2D) g;
+		g2d.setColor(Color.RED);
+		g2d.fillRect(190, 100, 50, 200);
+		;
+	}
+	
 	public static void main(String[] args) {
 		JFrame frame = new JFrame("Cricket");
-		frame.setSize(450,450);
+		frame.add(new Cricket());
+		frame.setSize(500, 500);
 		frame.setVisible(true);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	}
-}
+		
+	}
